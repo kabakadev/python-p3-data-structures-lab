519f7ade45b8e063bfc64369cccd2451c200546e
Ian Kabaka <iankabaka1@gmail.com>
2024-11-24 21:29:26 +0300

    tests passing for data structures p3 data strucutres lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,46 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [food["name"] for food in spicy_foods]
+    return names
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    heat_level = [food for food in spicy_foods if food["heat_level"] > 5]
+    return heat_level
+    
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: " + ('🌶' * food['heat_level']) )
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
+        
+
     pass
 
 def print_spiciest_foods(spicy_foods):
+   
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+           print(f"{food['name']} ({food['cuisine']}) | Heat Level: " + ('🌶' * food['heat_level']) )
+
     pass
 
 def get_average_heat_level(spicy_foods):
-    pass
+    counter = 0
+    num = len(spicy_foods)
+    for food in spicy_foods:
+        counter += food["heat_level"]
+    average = counter / num
+    return average
+    
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
